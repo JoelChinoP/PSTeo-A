ab5a49ecd0df16b5b73c20969660e3b7d0973726
Jose Luis Cusilayme Garcia <jcusilaymeg@unsa.edu.pe>
2024-06-20 18:03:06 -0500

    Resolucion del ejercicio 03

---
+++ GUIA_06/Ex03/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+using namespace std;
+
+
+int main() {
+    string frase;
+    ofstream outfile("frases.txt");
+
+
+    if (!outfile) {
+        cerr << "No se pudo abrir el archivo para escritura." << endl;
+        return 1;
+    }
+
+
+    cout << "Teclee frases (pulse Enter sin teclear nada para finalizar):" << endl;
+    while (true) {
+        getline(cin, frase);
+        if (frase.empty()) {
+            break; // Finaliza si el usuario pulsa Enter sin teclear nada
+        }
+        outfile << frase << endl;
+    }
+
+
+    outfile.close();
+
+
+    ifstream infile("frases.txt");
+    if (!infile) {
+        cerr << "No se pudo abrir el archivo para lectura." << endl;
+        return 1;
+    }
+
+
+    cout << "\nContenido del fichero 'frases.txt':" << endl;
+    while (getline(infile, frase)) {
+        cout << frase << endl;
+    }
+
+
+    infile.close();
+    return 0;
+}
