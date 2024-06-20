80276c89c980046609d17adab57a26d7213f0f7b
Jose Luis Cusilayme Garcia <jcusilaymeg@unsa.edu.pe>
2024-06-20 18:07:08 -0500

    Modificando el código para que no se sobreescriba, si no que agregue frases al final

--- GUIA_06/Ex03/main.cpp
+++ GUIA_06/Ex03/main.cpp
@@ -2,20 +2,16 @@
 #include <fstream>
 #include <string>
 
-
 using namespace std;
-
 
 int main() {
     string frase;
-    ofstream outfile("frases.txt");
-
+    ofstream outfile("frases.txt", ios::app); // Abrir en modo de añadir al final del archivo
 
     if (!outfile) {
         cerr << "No se pudo abrir el archivo para escritura." << endl;
         return 1;
     }
-
 
     cout << "Teclee frases (pulse Enter sin teclear nada para finalizar):" << endl;
     while (true) {
@@ -26,9 +22,7 @@ int main() {
         outfile << frase << endl;
     }
 
-
     outfile.close();
-
 
     ifstream infile("frases.txt");
     if (!infile) {
@@ -36,12 +30,10 @@ int main() {
         return 1;
     }
 
-
     cout << "\nContenido del fichero 'frases.txt':" << endl;
     while (getline(infile, frase)) {
         cout << frase << endl;
     }
-
 
     infile.close();
     return 0;
